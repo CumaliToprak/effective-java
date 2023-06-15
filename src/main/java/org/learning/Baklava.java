b5111cf2b4b2e12750265dce0d9412c53109d1c9
cumalitoprak <cumalitoprakk@gmail.com>
2023-06-16 00:50:50 +0300

    feature(builder): part2 add inner class in base class

--- src/main/java/org/learning/Baklava.java
+++ src/main/java/org/learning/Baklava.java
@@ -34,4 +34,70 @@ public class Baklava {
                 ", priceAsEuro=" + priceAsEuro +
                 '}';
     }
+
+    public static class Builder{
+        //static inner class
+        //static inner classes can be directly accessed using new BaklavaBuilder.Greece()
+        public static class Greece{
+            private int kg;
+            private String origin;
+            private BigDecimal priceAsEuro; //yunanistan
+
+            public Greece setKg(int kg){
+                this.kg = kg;
+                return this;
+            }
+
+            public Greece setOrigin(String origin)
+            {
+                this.origin = origin;
+                return this;
+            }
+
+            public Greece setPriceAsEuro(BigDecimal priceAsEuro)
+            {
+                this.priceAsEuro = priceAsEuro;
+                return this;
+            }
+
+            public Baklava build(){
+                return new Baklava(this.kg, this.origin, this.priceAsEuro);
+            }
+        }
+
+        public static class Turkey{
+            private int kg;
+            private String origin;
+            private BigDecimal priceAsEuro;
+            private BigDecimal exchangeRate;
+
+            public Turkey setKg(int kg){
+                this.kg = kg;
+                return this;
+            }
+
+            public Turkey setOrigin(String origin)
+            {
+                this.origin = origin;
+                return this;
+            }
+
+            public Turkey setExchangeRate(BigDecimal exchangeRate)
+            {
+                this.exchangeRate = exchangeRate;
+                return this;
+            }
+
+            public Turkey setPriceAsEuro(BigDecimal priceAsEuro)
+            {
+                this.priceAsEuro = priceAsEuro;
+                return this;
+            }
+
+            public Baklava build(){
+                return new Baklava(this.kg, this.origin, this.priceAsEuro, this.exchangeRate);
+            }
+        }
+    }
+
 }
--- src/main/java/org/learning/Main.java
+++ src/main/java/org/learning/Main.java
@@ -10,11 +10,19 @@ public class Main {
       /*Baklava baklava = new BaklavaBuilder().setKg(1).setOrigin("tr").build();
         System.out.println(baklava);*/
 
-        Baklava greekBaklava = new BaklavaBuilder.Greece().setKg(1).setOrigin("GREECE").setPriceAsEuro(BigDecimal.valueOf(5.44234)).build();
+        /*Baklava greekBaklava = new BaklavaBuilder.Greece().setKg(1).setOrigin("GREECE").setPriceAsEuro(BigDecimal.valueOf(5.44234)).build();
         System.out.println(greekBaklava);
 
         Baklava turkishBaklava = new BaklavaBuilder.Turkey().setKg(2).setPriceAsEuro(BigDecimal.valueOf(10)).setOrigin("TR").setExchangeRate(BigDecimal.valueOf(25.3)).build();
+        System.out.println(turkishBaklava);*/
+
+        Baklava greeceBaklava = new Baklava.Builder.Greece().setKg(1).setOrigin("GR").setPriceAsEuro(BigDecimal.valueOf(12.21)).build();
+        Baklava turkishBaklava = new Baklava.Builder.Turkey().setKg(1).setOrigin("GR").setPriceAsEuro(BigDecimal.valueOf(12.21)).
+        setExchangeRate(BigDecimal.valueOf(25.2)).build();
+
+        System.out.println(greeceBaklava);
         System.out.println(turkishBaklava);
+
 
     }
 }
